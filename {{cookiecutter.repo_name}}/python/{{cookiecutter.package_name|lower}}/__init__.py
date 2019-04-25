c630571e16734f33094a8c482098337b3f590cca
Andy Casey <andycasey@gmail.com>
2019-04-25 15:04:53 -0400

    fix pyyaml 5.1 issue

--- {{cookiecutter.repo_name}}/python/{{cookiecutter.package_name|lower}}/__init__.py
+++ {{cookiecutter.repo_name}}/python/{{cookiecutter.package_name|lower}}/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import distutils.version
 import os
 
 import yaml
@@ -28,12 +29,21 @@ NAME = '{{cookiecutter.package_name}}'
 
 
 # Loads config
-config = yaml.load(open(os.path.dirname(__file__) + '/etc/{0}.yml'.format(NAME)))
+yaml_kwds = dict()
+yaml_version = distutils.version.StrictVersion(yaml.__version__)
+if yaml_version >= distutils.version.StrictVersion('5.1'):
+    yaml_kwds.update(Loader=yaml.FullLoader)
+
+config_path = os.path.join(os.path.dirname(__file__), '/etc/{0}.yml'.format(NAME))
+with open(config_path, "r") as fp:
+    config = yaml.load(fp, **yaml_kwds)
 
 # If there is a custom configuration file, updates the defaults using it.
-custom_config_fn = os.path.expanduser('~/.{0}/{0}.yml'.format(NAME))
-if os.path.exists(custom_config_fn):
-    config = merge(yaml.load(open(custom_config_fn)), config)
+custom_config_path = os.path.expanduser('~/.{0}/{0}.yml'.format(NAME))
+if os.path.exists(custom_config_path):
+    with open(custom_config_path, "r") as fp:
+        custom_config = yaml.load(custom_config_path, **yaml_kwds)
+    config = merge(custom_config, config)
 
 
 __version__ = '{{cookiecutter.version}}'
